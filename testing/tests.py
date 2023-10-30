c20cd6b13a4c964bc717c713fb924c8c0208f18c
kyairie <justaboxofstuff@gmail.com>
2023-10-31 10:15:45 +1030

    added test cases

--- testing/tests.py
+++ testing/tests.py
@@ -2,34 +2,37 @@ import json
 import requests
 
 # rn this is just requesting existing microservices,
-# need to add creation and deletion of services
+# add creation and deletion of services? or manual
 
 def test_microservice(endpoint, parameters=None):
 
-    base_url = 'http://127.0.0.1:34225'
+    base_url = 'http://127.0.0.1:36107/'
     url = f'{base_url}/{endpoint}'
-
-    # Make a request with parameters
-    # try except
-    response = requests.get(url, params=parameters)
 
     # Check the response
     print(f"Testing request on {url} with parameters: {parameters}")
 
-    if response.status_code == 200:
-        print("Request was successful")
+    # Try making a request to url endpoint
+    try:
+        response = requests.get(url, params=parameters)
 
-        # https://www.w3schools.com/python/ref_requests_response.asp
+        if response.status_code == 200:
+            print("Request was successful")
 
-        print("Response body:", response)
-    else:
-        print("Request failed")
-        print("Status code:", response.status_code)
+            # https://www.w3schools.com/python/ref_requests_response.asp
+
+            print("Response body:", response.content)
+        else:
+            print("Request failed, status code:", response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
 
 # Load test data from the JSON file
 with open('tests.json') as json_file:
     tests = json.load(json_file)
-
+  
 # Run tests
 for test in tests:
     test_microservice(test['endpoint'], test['parameters'])
